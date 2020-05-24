b6d514c5afe1a7b16033b8232ff0ff8b103e7af3
Adam Russell <adam.russell@live.com>
2020-05-24 09:44:22 -0400

    Initial ports of some match classes

---
+++ darwin-csharp/Darwin/Matching/RefPoint.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Darwin.Matching
+{
+    public class RefPoint
+    {
+        public double Parameter { get; set; }
+        public int SegId { get; set; } // index of segment
+        public int SegOp { get; set; } // operation (0 means add, 1 means remove from active segment list)
+    }
+}
