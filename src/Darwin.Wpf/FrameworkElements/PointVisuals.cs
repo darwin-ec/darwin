418b3f9568bbbfafbc08064da47457d3ffc9bfbf
morgan-a-bauer <131829467+morgan-a-bauer@users.noreply.github.com>
2023-06-14 13:37:36 -0400

    Better comments

--- src/Darwin.Wpf/FrameworkElements/PointVisuals.cs
+++ src/Darwin.Wpf/FrameworkElements/PointVisuals.cs
@@ -115,7 +115,7 @@ namespace Darwin.Wpf.FrameworkElements
         public double PointSize
         {
             set { SetValue(PointSizeProperty, value); }
-            //GetValue(PointSizeProperty) returns ZoomRatio and uses it as a scale factor for a consistent point size across varied image resolutions
+            //GetValue(PointSizeProperty) returns OriginalZoomRatio and uses it as a scale factor for a consistent point size across varied image resolutions and zoom factors
             get { return 1.5 / (double)GetValue(PointSizeProperty); }
         }
 
